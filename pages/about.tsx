c65dd73af0d0fd25f1259f77ce4cbda5bdfdcaab
eduardolira <eduardolira@kali.lira>
2022-01-09 23:12:33 -0300

    netlify deploy

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import { Article } from "@components/Article"
+
+function about() {
+    return (
+        <Article>
+            <h1>Sobre mim e este blog</h1>
+            <p>Este é o melhor blog de todos</p>
+        </Article>
+    )
+}
+export default about
